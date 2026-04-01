37ec48f4136bc426ce28e5531b2e7bf2107c1a2b
khyaati <khyaati132@gmail.com>
2026-04-02 04:47:55 +0530

    redirect fix

--- src/services/api.js
+++ src/services/api.js
@@ -4,10 +4,10 @@
  */
 class ApiService {
   constructor() {
-    //this.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api' ;
-     this.baseURL = process.env.NODE_ENV === 'production'
-       ? '/api'
-       : process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+    this.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+    // this.baseURL = process.env.NODE_ENV === 'production'
+    //   ? '/api'
+    //   : process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
     this.timeout = 10000;
     console.log("baseURL: ", this.baseURL)
   }
